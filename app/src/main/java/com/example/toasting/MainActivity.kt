6712b5f0521b889a6b32e060135348d76d5d2ce6
Mohammad rasool <muhammedjaf33@gmail.com>
2025-02-03 01:27:28 +0300

    third commit

--- app/src/main/java/com/example/toasting/MainActivity.kt
+++ app/src/main/java/com/example/toasting/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.toasting
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.toasting.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
@@ -19,9 +21,37 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.button.setOnClickListener(){
-            val Intent=Intent(this,MainActivity2::class.java)
-            startActivity(Intent)
+            val intent=Intent(this,MainActivity2::class.java)
+            startActivity(intent)
         }
     }
+    override fun onStart(){
+        super.onStart()
+        Log.d("MainActivity","onStart")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity","onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity","onResume")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity","onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity","onDestroy")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity","onRestart")
+    }
+
 
 }
--- app/src/main/java/com/example/toasting/MainActivity2.kt
+++ app/src/main/java/com/example/toasting/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.toasting
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +12,32 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+    }
+    override fun onStart(){
+        super.onStart()
+        Log.d("MainActivity 2","onStart")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity2","onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity2","onResume")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity2","onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity2","onDestroy")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity2","onRestart")
     }
 }
